39573948774ee59d6a2d97b7c3c7c372b77fc570
jonirenko <93704954+jonirenko@users.noreply.github.com>
2025-04-22 14:23:41 +0300

    Added Music-page

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 function Music() {
   return (
-    <div>Music</div>
+    <div className='music-container'>
+      <div className='music'>
+        <div className='music-text'>
+          <h1>My Skills In Music</h1>
+
+          <h2>Songwriting</h2>
+          <p>
+            jghdogfogiojgdpgjfsiojfdjgö Children of the Frost gudsogdaivdao Last Winter's Child ect.
+          </p>
+
+          <h2>Music Production</h2>
+          <p>
+            gfdspbnsvndoavndsöfn mixing and mastering fdiasvidnvbosn Reaper gdasog
+          </p>
+
+          <h2>Music Promotion</h2>
+          <p>
+            gdsbvdosanvdospabf social media gdoshbosdbnvosdvbn content creation gdfusobgso
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
