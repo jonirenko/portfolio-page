6294c356d22c7ca52d2be264afa29d38a90c3ade
jonirenko <93704954+jonirenko@users.noreply.github.com>
2025-04-28 19:21:53 +0300

    cleanup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Navbar() {      
   return (
     <nav className='navbar'>
         <div className='navbar-container'>
